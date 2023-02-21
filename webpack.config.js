d0b5cacd50485b2354db38b2c3bdbc2b4c2ecb62
artursantana <artursantana843@gmail.com>
2023-02-21 10:46:07 +0000

    Alterando para pasta Docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.jsx',
     output : {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
     },
 
